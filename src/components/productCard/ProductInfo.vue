da1fe5eeb5d54a36b21df9596b4eac8853960019
Dn_Sh <shipilovdanil0@gmail.com>
2025-04-14 22:09:03 +0500

    express and mongodb

---
+++ src/components/productCard/ProductInfo.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="card__product-info">
+        <div class="card__type"><RegularText>{{ type }}</RegularText></div>
+        <div class="card__art">Art: {{ id }}</div>
+    </div>
+</template>
+
+<script setup>
+import RegularText from '../text/RegularText.vue'
+const props = defineProps({
+    type: String,
+    id: Number, //change to string
+})
+</script>
+
+<style scoped>
+
+.card__product-info {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+}
+
+.card__art {
+    font-weight: 400;
+    font-size: 14px;
+    color: var(--gray-dark-text);
+    opacity: 0.7;
+}
+
+</style>
