76c40b5ec836cad7dd0a0ab35ee5911d556ccf28
dnflnx <75557947+flionx@users.noreply.github.com>
2025-05-02 17:15:44 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,10 +14,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({
     origin: [
-        'http://localhost:5173',
-        'http://localhost:5174',
-        'https://trendy-young.vercel.app/'
-        // domain <----
+        // 'http://localhost:5173',
+        // 'http://localhost:5174',
+        'https://trendy-young.vercel.app'
     ],
     credentials: true,
 }))
